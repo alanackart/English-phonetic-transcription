01366aeec165af7ec0769a0664dc6b621d677b5b
alanackart <alanackart@gmail.com>
2017-04-25 21:15:07 +0800

    Create dictionary_IPA.cs

---
+++ dictionary_IPA.cs
@@ -0,0 +1,17 @@
+ Dictionary<string, string> ipa =
+           new Dictionary<string, string>();
+
+            ipa.Add("&", "æ");
+            ipa.Add(".", "ˌ");
+            ipa.Add("U", "ʊ");
+            ipa.Add("S", "ʃ");
+            ipa.Add("R", "r");
+            ipa.Add("O", "ɔː");
+            ipa.Add("Z", "ʒ");
+            ipa.Add("V", "ʌ");
+            ipa.Add("N", "ŋ");
+            ipa.Add("u", "uː");
+            ipa.Add("T", "θ");
+            ipa.Add("D", "ð");
+            ipa.Add("i", "iː");
+            ipa.Add("A", "ɑː");
